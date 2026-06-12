6e104de2d443b7c2e0cf7fdfa55d17fa23b3034a
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-06-12 17:23:46 +0530

    euclidean gcd

--- Gcd.java
+++ Gcd.java
@@ -1,10 +1,18 @@
 public class Gcd {
     public static int gcd(int n1,int n2){
-        int gcd = 1;
-        for(int i = 1;i<=Math.min(n2, n1);i++){
-            gcd = i;
+        // int gcd = 1;
+        // for(int i = 1;i<=Math.min(n2, n1);i++){
+        //     gcd = i;
+        // }
+        // return gcd;
+
+        // Euclidean Algorithm
+        while(n1>0 && n2>0){
+            if(n1>n2) n1%=n2;
+            else n2%=n1;
         }
-        return gcd;
+        if(n1==0) return n2;
+        return n1;
     }
 
     public static void main(String[] args) {
