94de656d5becd4a8a7d838649cd18c30bb0fff16
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-06-22 20:37:19 +0530

    Move Zeros to End

---
+++ Striver/Arrays/Easy/MoveZerostoEnd.java
@@ -0,0 +1,33 @@
+package Striver.Arrays.Easy;
+
+import java.util.Arrays;
+
+public class MoveZerostoEnd {
+    public static void moveZeroes(int[] nums) {
+        int j = -1;
+        for(int i = 0; i < nums.length;i++){
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+        for(int i = j +1 ; i<nums.length;i++){
+            if (nums[i] != 0) {
+                swap(nums, i, j);
+                j++;
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,0,2,3,0,0,2,1};
+        moveZeroes(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
