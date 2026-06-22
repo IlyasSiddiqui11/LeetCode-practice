68c71bc1cef575fa6d2afc74ac0fbc83cc7c60f4
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-06-22 19:17:40 +0530

    Remove duplicates

---
+++ Striver/Arrays/Easy/RemoveDuplicates.java
@@ -0,0 +1,19 @@
+package Striver.Arrays.Easy;
+
+public class RemoveDuplicates {
+    public static int removeDuplicates(int[] nums){
+        int p = 0;
+        for(int i = 1;i<nums.length;i++){
+            if (nums[i] != nums[p]) {
+                p++;
+                nums[p] = nums [i];
+            }
+        }
+        return p+1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr= {1,1,1,2,4,4,4,5,5};
+        System.out.println(removeDuplicates(arr));
+    }
+}
