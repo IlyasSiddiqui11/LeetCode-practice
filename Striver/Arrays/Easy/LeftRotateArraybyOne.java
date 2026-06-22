bbb4876729490728864044628ebcf4715db82ecf
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-06-22 19:31:00 +0530

    Left Rotate Array by One

---
+++ Striver/Arrays/Easy/LeftRotateArraybyOne.java
@@ -0,0 +1,19 @@
+package Striver.Arrays.Easy;
+
+import java.util.Arrays;
+
+public class LeftRotateArraybyOne {
+    public static void rotateArrayByOne(int[] nums){
+        int a = nums[0];
+        for(int i = 1;i<nums.length;i++){
+            nums[i-1] = nums[i];
+        }
+        nums[nums.length-1] = a;
+        System.out.println(Arrays.toString(nums));
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        rotateArrayByOne(arr);
+    }
+}
