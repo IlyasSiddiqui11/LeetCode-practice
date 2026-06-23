93c60f0766d77243c91fb9fc6e0c6f611f5b903d
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-06-23 21:38:53 +0530

    Max Consecutive Ones

---
+++ Striver/Arrays/Easy/MaxConsecutiveOnes.java
@@ -0,0 +1,24 @@
+package Striver.Arrays.Easy;
+
+public class MaxConsecutiveOnes {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+        for(int i = 0;i<nums.length;i++){
+            if (nums[i] == 1) {
+                count++;
+                if (count > max) {
+                    max++;
+                }
+            }
+            else
+                count = 0;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,0,1,1,1,0};
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+}
