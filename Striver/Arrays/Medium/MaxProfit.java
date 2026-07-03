a3f25374532112e9e4fdede9af02af615cc29767
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-07-03 19:19:39 +0530

    Best Time To Buy & Sell Stock

--- Striver/Arrays/Medium/MaxProfit.java
+++ Striver/Arrays/Medium/MaxProfit.java
@@ -1,16 +1,6 @@
+package Striver.Arrays.Medium;
 
 public class MaxProfit {
-    public static int maxProfitII(int[] prices) {
-        int profit = 0;
-        for (int i = 0; i < prices.length - 1; i++) {
-            if (prices[i] < prices[i + 1]) {
-                profit = prices[i] - prices[i + 1];
-            }
-        }
-        System.out.println(profit);
-        return profit;
-    }
-
     public static int maxProfit(int[] prices) {
         int buy = prices[0];
         int profit = 0;
