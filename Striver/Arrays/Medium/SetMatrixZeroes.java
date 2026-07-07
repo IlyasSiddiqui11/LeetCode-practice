49aa015fd03e42477696e334d64bc3232fe6823c
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-07-07 15:29:51 +0530

    Set Matrix Zeroes

---
+++ Striver/Arrays/Medium/SetMatrixZeroes.java
@@ -0,0 +1,35 @@
+// package Striver.Arrays.Medium;
+
+public class SetMatrixZeroes {
+    public static void setZeroes(int[][] matrix) {
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];  
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }   
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(row[i] == 1 || col[j] == 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+        setZeroes(matrix);
+        System.out.println(java.util.Arrays.deepToString(matrix));
+    }
+}
