5303500fe22f2a9eb65230fbc4d61eeee0f5dcab
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-07-08 19:43:45 +0530

    Subarray Sum Equals K

--- Striver/Arrays/Medium/SubarraySumEqualsK.java
+++ Striver/Arrays/Medium/SubarraySumEqualsK.java
@@ -4,17 +4,19 @@ public class SubarraySumEqualsK {
     // Brute Force Approach
     // Time Complexity: O(n^2)
     // Space Complexity: O(1)
-    public static int subarraySum(int[] nums, int k) {
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                if (sum == k) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
+    // public static int subarraySum(int[] nums, int k) {
+    //     int count = 0;
+    //     for (int i = 0; i < nums.length; i++) {
+    //         int sum = 0;
+    //         for (int j = i; j < nums.length; j++) {
+    //             sum += nums[j];
+    //             if (sum == k) {
+    //                 count++;
+    //             }
+    //         }
+    //     }
+    //     return count;
+    // }
+
+    
 }
