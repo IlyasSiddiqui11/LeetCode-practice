db80a9b6bb5376a55558a85907b604ad2aba8842
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-07-13 19:47:30 +0530

    Subarray Sum Equals K

--- Striver/Arrays/Medium/SubarraySumEqualsK.java
+++ Striver/Arrays/Medium/SubarraySumEqualsK.java
@@ -1,4 +1,7 @@
-package Striver.Arrays.Medium;
+// package Striver.Arrays.Medium;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 public class SubarraySumEqualsK {
     // Brute Force Approach
@@ -17,4 +20,24 @@ public class SubarraySumEqualsK {
     //     }
     //     return count;
     // }
+
+    public static int subarraySum(int[] nums, int k){
+        Map<Integer,Integer> mp = new TreeMap<>();
+        int cnt = 0, prefixSum = 0;
+        mp.put(0, 1);
+        for(int i = 0;i<nums.length;i++){
+            prefixSum += nums[i];
+            int remove = prefixSum - k;
+            if (mp.containsKey(remove)) {
+                cnt += mp.get(remove);
+            }
+            mp.put(prefixSum, mp.getOrDefault(prefixSum,0)+1);
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,-3,1,1,1,4,2,-3};
+        System.out.println(subarraySum(arr, 3));
+    }
 }
