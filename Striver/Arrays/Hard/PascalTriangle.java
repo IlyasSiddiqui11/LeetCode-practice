55c668a6cb85f493c432b6e6cad07f07e652587b
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-07-14 21:06:56 +0530

    Pascal's Triangle

--- Striver/Arrays/Hard/PascalTriangle.java
+++ Striver/Arrays/Hard/PascalTriangle.java
@@ -1,22 +1,26 @@
 // package Striver.Arrays.Hard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PascalTriangle {
 
-    public static int factorial(int n){
-        if (n==0) {
+    public static int factorial(int n) {
+        if (n == 0) {
             return 1;
         }
-        return n * factorial(n-1);
+        return n * factorial(n - 1);
     }
-    public static int generate1(int row, int column){
-        //Brute
+
+    public static int nCr(int row, int column) {
+        // Brute
         int n = row - 1;
         int r = column - 1;
         // return factorial(n)/(factorial(r)*factorial(n - r));
 
-        //Better 
+        // Better
         int res = 1;
-        for(int i = 0;i < r;i++){
+        for (int i = 0; i < r; i++) {
             res = res * (n - i);
             res = res / (i + 1);
         }
@@ -24,21 +28,34 @@ public class PascalTriangle {
         return res;
     }
 
-    public static void generate2(int row){
+    public static List<Integer> generate2(int row) {
+        List<Integer> result = new ArrayList<>();
         // Brute
         // for(int c = 1;c<=row;c++){
-        //     System.out.println(generate1(row, c));
+        // System.out.println(nCr
+        // (row, c));
         // }
-    
+
         int ans = 1;
-        System.out.println(ans);
-        for(int i = 1; i<row;i++){
-            ans = (ans * (row -i)) / i;
-            System.out.println(ans);
+        result.add(ans);
+        for (int i = 1; i < row; i++) {
+            ans = (ans * (row - i)) / i;
+            result.add(ans);
         }
+        return result;
     }
 
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        for (int row = 1; row <= numRows; row++) {
+            ans.add(generate2(row));
+        }
+        return ans;
+    }
+
+
     public static void main(String[] args) {
-        generate2(5);
+        System.out.println(generate(5));
+
     }
 }
