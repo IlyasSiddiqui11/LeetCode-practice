aca1f1eebceaa9c9137bf8be3e753bb5c859cadc
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-07-22 21:22:43 +0530

    Count Subarray With Xor K

---
+++ Striver/Arrays/Hard/CountSubarraywithXorK.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class CountSubarraywithXorK {
+    public static int subarrayXorK(int[] arr, int k){
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        int prefixXor = 0, cnt = 0;
+        mpp.put(0, 1);
+        for(int i = 0; i < arr.length; i++){
+            prefixXor ^= arr[i];
+            int x = prefixXor^k;
+            if (mpp.containsKey(x)) {
+                cnt += mpp.get(x);
+            }
+            mpp.put(prefixXor, mpp.getOrDefault(prefixXor, 0)+1);
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,2,2,6,4};
+        System.out.println(subarrayXorK(arr, 6));
+    }
+} 
