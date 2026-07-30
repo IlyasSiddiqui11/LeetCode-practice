86da99b566f1ea49f2c06090d0251bf4e384fe2e
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-07-30 18:57:55 +0530

    Find First and Last Position of Element in Sorted Array

--- Striver/Binary Search/On 1D Arrays/FirstAndLastPosition.java
+++ Striver/Binary Search/On 1D Arrays/FirstAndLastPosition.java
@@ -33,12 +33,54 @@ public class FirstAndLastPosition {
         return ans;
     }
 
-    public static int[] searchRange(int[] nums, int target) {
+    public static int[] searchRangeI(int[] nums, int target) {
         int lb = lowerBound(nums, target);
         if (lb == nums.length || nums[lb] != target) {
             return new int[] {-1,-1}; 
         }
         return new int[] {lb, upperBound(nums, target) - 1};
+    }
+
+    private static int firstOccurrence(int[] nums, int target){
+        int first = -1;
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                high = mid - 1;
+                first = mid;
+            } 
+            else if (nums[mid] > target) {
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return first;
+    }
+
+    private static int lastOccurrence(int[] nums, int target){
+        int last = -1;
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                low = mid + 1;
+                last = mid;
+            } 
+            else if (nums[mid] > target) {
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return last;
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        return new int[] {firstOccurrence(nums, target), lastOccurrence(nums, target)};
     }
 
     public static void main(String[] args) {
