f67988ad0a5d2407b24aafa5e1530eb543930e13
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-08-05 12:15:04 +0530

    Find Minimum in Rotated Sorted Array

--- Striver/Binary Search/On 1D Arrays/FindMinimuminRotatedSortedArray.java
+++ Striver/Binary Search/On 1D Arrays/FindMinimuminRotatedSortedArray.java
@@ -1,5 +1,20 @@
 public class FindMinimuminRotatedSortedArray {
     public int findMin(int[] nums) {
-        
+        int start = 0, end = nums.length - 1, ans = Integer.MAX_VALUE;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[start] <= nums[end]) {
+                ans = Math.min(ans, nums[start]);
+            }
+            if (nums[start] <= nums[mid]) {
+                ans = Math.min(ans, nums[start]);
+                start = mid + 1;
+            }
+            else{
+                ans = Math.min(ans, nums[mid]);
+                end = mid - 1;
+            }
+        }
+        return ans;
     }
 }
