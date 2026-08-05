7eec8d583ec77ce3d93933b83fbe6d174054ecdf
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-08-05 12:32:10 +0530

    Find out how many times array has been rotated

--- Striver/Binary Search/On 1D Arrays/HowManyTimesArrayIsRotated.java
+++ Striver/Binary Search/On 1D Arrays/HowManyTimesArrayIsRotated.java
@@ -1,27 +1,27 @@
 public class HowManyTimesArrayIsRotated {
     public static int findKRotation(int[] nums) {
-        int start = 0, end = nums.length - 1, ans = Integer.MAX_VALUE, index = -1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[start] <= nums[end]) {
-                if (nums[start] < ans) {
-                    index = start;
-                    ans = nums[start];
+        int low = 0, high = nums.length - 1, ans = Integer.MAX_VALUE, index = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[low] <= nums[high]) {
+                if (nums[low] < ans) {
+                    index = low;
+                    ans = nums[low];
                 }
             }
-            if (nums[start] <= nums[mid]) {
-                if (nums[start] < ans) {
-                    index = start;
-                    ans = nums[start];
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] < ans) {
+                    index = low;
+                    ans = nums[low];
                 }
-                start = mid + 1;
+                low = mid + 1;
             }
             else{
                 if (nums[mid] < ans) {
                     index = mid;
                     ans = nums[mid];
                 }
-                end = mid - 1;
+                high = mid - 1;
             }
         }
         return index;
