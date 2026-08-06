65f988191eee748ef5d8aa46731c22ae6ccafa07
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-08-06 21:56:10 +0530

    Koko Eating Bananas

--- Striver/Binary Search/On Answers/KokoEatingBananas.java
+++ Striver/Binary Search/On Answers/KokoEatingBananas.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 public class KokoEatingBananas {
+    private static long totalHours(int[] piles, int hourly) {
+        long totalHours = 0;
+        for (int pile : piles) {
+            totalHours += (int) Math.ceil((double) pile / hourly);
+        }
+        return totalHours;
+    }
+
     public static int minEatingSpeed(int[] piles, int h) {
+        int maxPile = Arrays.stream(piles).max().getAsInt();
         
+        int low = 1, high = maxPile, ans = maxPile;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (totalHours(piles, mid) <= h) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid  + 1;
+            }
+        }
+        return ans;
     }
 }
