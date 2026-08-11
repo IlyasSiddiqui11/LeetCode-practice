a6367c3f1ce561a57bad0d920cf53dab01738ef0
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-08-11 15:57:46 +0530

    Find the smallest divisor

--- Striver/Binary Search/On Answers/FindtheSmallestDivisor.java
+++ Striver/Binary Search/On Answers/FindtheSmallestDivisor.java
@@ -1,5 +1,31 @@
 public class FindtheSmallestDivisor {
-    public int smallestDivisor(int[] nums, int threshold) {
-        
+    private static int sumByD(int[] nums, int div){
+        int sum = 0;
+        for(int num : nums){
+            sum += Math.ceil((double)num / div);
+        }
+        return sum;
+    }
+    public static int smallestDivisor(int[] nums, int threshold) {
+        int minValue = Integer.MAX_VALUE;
+        int maxValue = Integer.MIN_VALUE;
+        for (int num : nums) {
+            minValue = Math.min(minValue, num);
+            maxValue = Math.max(maxValue, num);
+        }
+        int low = minValue, high = maxValue;
+        int ans = maxValue;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (sumByD(nums, mid) <= threshold) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 }
