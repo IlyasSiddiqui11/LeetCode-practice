84eef340f5dea539fcd7e75cbab7c5684c5cf23d
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-08-12 13:09:30 +0530

    Capacity To Ship Packages Within D Days

--- Striver/Binary Search/On Answers/CapacityToShipPackagesWithinDDays.java
+++ Striver/Binary Search/On Answers/CapacityToShipPackagesWithinDDays.java
@@ -1,3 +1,33 @@
+import java.util.Arrays;
+
 public class CapacityToShipPackagesWithinDDays {
-    
+    private static int  neededDays(int[] weights, int capacity){
+        int sum = 0, days = 1;
+        for(int weight : weights){
+            if (sum + weight > capacity) {
+                days++;
+                sum = weight;
+            }
+            else{
+                sum += weight;
+            }
+        }
+        return days;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int low = Arrays.stream(weights).max().getAsInt();
+        int high = Arrays.stream(weights).sum();
+        int ans = high;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if (neededDays(weights, mid) <= days) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
 }
