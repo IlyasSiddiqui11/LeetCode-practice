3da7d730a500b0b033132a2b1c71761c261fbd22
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-08-14 21:05:53 +0530

    Kth Missing Positive Number

--- Striver/Binary Search/On Answers/KthMissingPositiveNumber.java
+++ Striver/Binary Search/On Answers/KthMissingPositiveNumber.java
@@ -9,8 +9,23 @@ public class KthMissingPositiveNumber {
         return k;
     }
 
+    public static int findKthPositive(int[] arr, int k){
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int missing = arr[mid] - (mid + 1);
+            if (missing < k) {
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return high + 1 + k;
+    }
+
     public static void main(String[] args) {
         int[] arr = {5,7,10,12};
-        System.out.println(findKthPositiveBrute(arr, 6));
+        System.out.println(findKthPositive(arr, 6));
     }
 }
