dfcdec5d1421a24bb09197842b63cec893dd7d71
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-08-18 19:07:05 +0530

    Finding Sqrt of a number using Binary Search

--- Striver/Binary Search/On Answers/FindSquareRootOfANumber.java
+++ Striver/Binary Search/On Answers/FindSquareRootOfANumber.java
@@ -16,6 +16,6 @@ public class FindSquareRootOfANumber {
         return ans;
     }
     public static void main(String[] args) {
-        System.out.println(squareRoot(23));
+        System.out.println(squareRoot(36));
     }
 }
