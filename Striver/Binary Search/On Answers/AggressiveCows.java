79a1f1b35ac29a41aa0f1ed59ea0bd5b717ddcee
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-08-19 21:19:28 +0530

    Aggressive Cows

--- Striver/Binary Search/On Answers/AggressiveCows.java
+++ Striver/Binary Search/On Answers/AggressiveCows.java
@@ -1,3 +1,26 @@
+import java.util.Arrays;
+
 public class AggressiveCows {
-    
+    private boolean canWePlace(int[] stalls, int cows, int distance){
+        int cntCows = 1, last = stalls[0];
+        for (int i = 1; i < stalls.length - 1; i++) {
+            if (stalls[i] - last >= distance) {
+                cntCows++;
+                last = stalls[i];
+            }
+        }
+        return cntCows >= cows;
+    }
+    public int aggressiveCowsBrute(int[] stalls, int cows){
+        Arrays.sort(stalls);
+        int max = Arrays.stream(stalls).max().getAsInt();
+        int min = Arrays.stream(stalls).min().getAsInt();
+        for(int i = 1; i < max - min; i++){
+            if (canWePlace(stalls, cows, i)) {
+                continue;
+            }
+            else return i - 1;
+        }
+        return max;
+    }
 }
