b004ac83c6777d5fb12992ff25e4e2fe86ece977
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-08-22 12:19:41 +0530

    Painter's Partition

---
+++ Striver/Binary Search/On Answers/PaintersPartition.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PaintersPartition{
+    public static int paintersPartition(ArrayList<Integer> arr, int k){
+        int low = Collections.max(arr);
+        int high = arr.stream().mapToInt(Integer::intValue).sum();
+        if (arr.size() < k) {
+            return -1;
+        }
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (cntPainters(arr, mid) > k) {
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+    private static int cntPainters(ArrayList<Integer> arr, int paint){
+        int cntpainters = 1;
+        long noOfPaints = 0;
+        for(int i = 0; i < arr.size(); i++){
+            if (noOfPaints + arr.get(i) <= paint) {
+                noOfPaints += arr.get(i);
+            }
+            else{
+                cntpainters++;
+                noOfPaints = arr.get(i);
+            }
+        }
+        return cntpainters; 
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(List.of(10,20,30,40));
+        System.out.println(paintersPartition(list, 2));
+    }
+}
