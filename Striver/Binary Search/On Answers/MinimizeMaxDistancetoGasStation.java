0ce7266110d3bf6b9405ca5155428b3266b02729
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-09-04 23:48:11 +0530

    Minimise Max Distance Between Gas Station

--- Striver/Binary Search/On Answers/MinimizeMaxDistancetoGasStation.java
+++ Striver/Binary Search/On Answers/MinimizeMaxDistancetoGasStation.java
@@ -1,3 +1,5 @@
+import java.util.PriorityQueue;
+
 public class MinimizeMaxDistancetoGasStation {
     public double minimiseMaxDistanceBrute(int[] arr, int k) {
         int[] howMany = new int[arr.length - 1];
@@ -24,4 +26,27 @@ public class MinimizeMaxDistancetoGasStation {
         }
         return maxAns;
     }
+
+    // Priority Queue
+    public double minimiseMaxDistanceBetter(int[] arr, int k) {
+        int[] howMany = new int[arr.length - 1];
+
+        for(int gasS = 1; gasS < k; gasS++){
+            // int diff = arr[i + 1] - arr[i];
+            // double secLen = diff / (double) howMany[i] + 1.0;
+        }
+
+        double maxAns = -1;
+        for(int i = 0; i < arr.length - 1; i++){
+            int diff = arr[i + 1] - arr[i];
+            double secLen = diff / (double) howMany[i] + 1.0;
+            maxAns = Math.max(maxAns, secLen);
+        }
+        return maxAns;
+    }
+
+
+
+    public static void main(String[] args) {
+        
 }
