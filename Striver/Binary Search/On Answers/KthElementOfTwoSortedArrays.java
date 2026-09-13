e9e8e74988bbd71a5682a89f518c9dfb1a47b664
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-09-13 19:23:42 +0530

    K-th Element of two sorted arrays

--- Striver/Binary Search/On Answers/KthElementOfTwoSortedArrays.java
+++ Striver/Binary Search/On Answers/KthElementOfTwoSortedArrays.java
@@ -1,47 +1,50 @@
 public class KthElementOfTwoSortedArrays {
-    public int kthElement(int[] a, int[] b, int k) {
+    public static int kthElement(int[] a, int[] b, int k) {
         if (a.length > b.length) {
-            return kthElement(b, a,k);
+            return kthElement(b, a, k);
         }
         int n = a.length;
         int m = b.length;
 
         int low = Math.max(0, k - m);
         int high = Math.min(k, n);
-        int leftSize = (n + m + 1) / 2;
+        int leftSize = k;
 
-        while(low <= high){
+        while (low <= high) {
             int partitionA = (low + high) / 2;
             int partitionB = leftSize - partitionA;
 
-            int leftA = (partitionA == 0)
-                    ? Integer.MIN_VALUE
-                    : a[partitionA - 1];
+            int leftA = partitionA > 0
+                    ? a[partitionA - 1]
+                    : Integer.MIN_VALUE;
 
-            int rightA = (partitionA == n)
-                    ? Integer.MAX_VALUE
-                    : a[partitionA];
+            int rightA = partitionA < n
+                    ? a[partitionA]
+                    : Integer.MAX_VALUE;
 
-            int leftB = (partitionB == 0)
-                    ? Integer.MIN_VALUE
-                    : b[partitionB - 1];
+            int leftB = partitionB > 0
+                    ? b[partitionB - 1]
+                    : Integer.MIN_VALUE;
 
-            int rightB = (partitionB == m)
-                    ? Integer.MAX_VALUE
-                    : b[partitionB];
+            int rightB = partitionB < m
+                    ? b[partitionB]
+                    : Integer.MAX_VALUE;
 
             if (leftA <= rightB && leftB <= rightA) {
-                if ((n + m) % 2 == 1) {
-                    return Math.max(leftA, leftB);
-                }
-            }
-            else if (leftA > rightB) {
+                return Math.max(leftA, leftB);
+            } else if (leftA > rightB) {
                 high = partitionA - 1;
-            }
-            else {
+            } else {
                 low = partitionA + 1;
             }
         }
         return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = { 2, 7, 10, 11 };
+        int[] nums2 = { 1, 2, 4, 5 };
+
+        System.out.println(kthElement(nums1, nums2, 2));
     }
 }
