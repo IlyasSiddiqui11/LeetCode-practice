dde0818a93051c284518fe8267cc4fd645e5944b
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-09-13 23:29:47 +0530

    Find the row with maximum numbers of 1s - Brute

--- Striver/Binary Search/On 2D Arrays/FindTheRowWithMaximumNumberOf1s.java
+++ Striver/Binary Search/On 2D Arrays/FindTheRowWithMaximumNumberOf1s.java
@@ -1,5 +1,30 @@
 public class FindTheRowWithMaximumNumberOf1s {
-    public int rowWithMax1s(int[][] matrix, int n, int m) {
-        
+    public static int rowWithMax1sBrute(int[][] matrix, int n, int m) {
+        int maxCnt = 0, index = -1;
+        for(int i = 0; i < n; i++){
+            int cnt = 0;
+            for(int j = 0; j < m; j++){
+                if (matrix[i][j] == 1) {
+                    cnt++;
+                }
+                if (cnt > maxCnt) {
+                    maxCnt = Math.max(maxCnt, cnt);
+                    index = i;
+                }
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {0, 0, 1, 1},
+                {0, 1, 1, 1},
+                {0, 0, 0, 1},
+                {1, 1, 1, 1}
+        };
+
+
+        System.out.println(rowWithMax1sBrute(matrix, 4, 4));
     }
 }
