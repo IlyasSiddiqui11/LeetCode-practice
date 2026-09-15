674a55ee96905ee14839aed0e8951b823691bd68
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-09-15 10:40:09 +0530

    Find the row with maximum numbers of 1s - Optimal

--- Striver/Binary Search/On 2D Arrays/FindTheRowWithMaximumNumberOf1s.java
+++ Striver/Binary Search/On 2D Arrays/FindTheRowWithMaximumNumberOf1s.java
@@ -16,6 +16,34 @@ public class FindTheRowWithMaximumNumberOf1s {
         return index;
     }
 
+    public static int lowerBound(int[] arr, int x){
+        int ans = arr.length;
+        int low = 0, high = arr.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= x){
+                high = mid - 1;
+                ans = mid;
+            }
+            else if (arr[mid] <= x ) {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int rowWithMax1s(int[][] matrix, int n, int m){
+        int maxCnt = 0, index = -1;
+        for(int i = 0; i < n; i++){
+            int cnt = m - lowerBound(matrix[i], 1);
+            if (cnt > maxCnt) {
+                maxCnt = cnt;
+                index = i;
+            }
+        }
+        return index;
+    }
+
     public static void main(String[] args) {
         int[][] matrix = {
                 {0, 0, 1, 1},
@@ -25,6 +53,6 @@ public class FindTheRowWithMaximumNumberOf1s {
         };
 
 
-        System.out.println(rowWithMax1sBrute(matrix, 4, 4));
+        System.out.println(rowWithMax1s(matrix, 4, 4));
     }
 }
