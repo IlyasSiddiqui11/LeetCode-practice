dad70c9db77c628bb04f5c31a7fb7dbd34a055fd
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-09-17 23:20:02 +0530

    Search in a 2D Matrix II

--- Striver/Binary Search/On 2D Arrays/SearchIn2DMatrixII.java
+++ Striver/Binary Search/On 2D Arrays/SearchIn2DMatrixII.java
@@ -1,4 +1,8 @@
 public class SearchIn2DMatrixII {
+
+    // Brute force will be searching linearly in the array. O(n * m)
+    // Better solution will be picking every row from the 2D matrix and performing binary search on that row. O(n * log m)
+
     public boolean searchMatrix(int[][] matrix, int target) {
         
     }
