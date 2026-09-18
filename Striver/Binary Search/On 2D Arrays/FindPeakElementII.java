65539e22422279a2a5074753087063a15d534caa
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-09-18 21:23:24 +0530

    Find Peak Element II

---
+++ Striver/Binary Search/On 2D Arrays/FindPeakElementII.java
@@ -0,0 +1,31 @@
+public class FindPeakElementII {
+    public int[] findPeakGrid(int[][] mat) {
+        int low = 0, high = mat.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) /2;
+            int row = maxElement(mat, mid);
+            int left = mid - 1 >= 0 ? mat[row][mid - 1] : -1;
+            int right = mid + 1 < mat[0].length ? mat[row][mid + 1] : -1;
+            
+            if (mat[row][mid] > left && mat[row][mid] > right) {
+                return new int[] {row,mid};
+            }
+            else if(mat[row][mid] < left) high = mid - 1;
+            else low = mid + 1;
+        }
+        return new int[] {-1,-1};
+    }
+
+    private int maxElement(int[][] mat, int col){
+        int max = Integer.MIN_VALUE;
+        int index = -1;
+
+        for(int i = 0; i < mat.length; i++){
+            if (mat[i][col] > max) {
+                max = mat[i][col];
+                index = i;
+            }
+        }
+        return index;
+    }
+}
