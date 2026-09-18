30cf6f241e453602bb7b32cc668fe6985964b74a
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-09-18 16:05:42 +0530

    Search in a 2D Matrix II - Optimal

--- Striver/Binary Search/On 2D Arrays/SearchIn2DMatrixII.java
+++ Striver/Binary Search/On 2D Arrays/SearchIn2DMatrixII.java
@@ -4,6 +4,22 @@ public class SearchIn2DMatrixII {
     // Better solution will be picking every row from the 2D matrix and performing binary search on that row. O(n * log m)
 
     public boolean searchMatrix(int[][] matrix, int target) {
-        
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int row = 0;
+        int col = m - 1;
+
+        while(row < n && col >= 0){
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            else if(matrix[row][col] > target){
+                col--;
+            } else{
+                row++;
+            }
+        }
+        return false;
     }
 }
