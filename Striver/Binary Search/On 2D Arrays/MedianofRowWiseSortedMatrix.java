c22f62ae854f5b8e29c27d23e1cacf58307531d7
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-09-19 17:15:41 +0530

    Median of Row Wise Sorted Matrix - Optimal

--- Striver/Binary Search/On 2D Arrays/MedianofRowWiseSortedMatrix.java
+++ Striver/Binary Search/On 2D Arrays/MedianofRowWiseSortedMatrix.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class MedianofRowWiseSortedMatrix {
-    
+
     public int findMedianBrute(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
@@ -14,7 +14,61 @@ public class MedianofRowWiseSortedMatrix {
             }
         }
         Collections.sort(ls);
-        return ls.get((n+m)/2);
+        return ls.get((n*m)/2);
     }
 
+    public static int findMedian(int[][] matrix){
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int low = matrix[0][0];
+        int high = matrix[0][cols - 1];
+
+        for(int i = 1; i < rows; i++){
+            low = Math.min(low, matrix[i][0]);
+            high = Math.max(high, matrix[i][cols - 1]);
+        }
+        int target = (rows * cols) / 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int smallerOrEqual = smallerOrEqual(matrix,mid);
+            if (smallerOrEqual <= target) {
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    private static int smallerOrEqual(int[][] matrix,int x){
+        int m = matrix[0].length;
+        int num = 0;
+        for(int i = 0; i < m; i++){
+            num += upperBound(matrix[i], x);
+        }
+        return num;
+    }
+
+    public static int upperBound(int[] arr, int x){
+        int ans = arr.length;
+        int low = 0, high = arr.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > x){
+                high = mid - 1;
+                ans = mid;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {{1,4,6},
+                        {8,10,12},
+                        {12,15,20}};
+        System.out.println(findMedian(matrix));
+    }
 }
