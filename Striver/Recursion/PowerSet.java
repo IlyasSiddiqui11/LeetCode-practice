c1b91b971a5e474ba3a1c27923e4d1a662adb5cf
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-09-22 15:45:49 +0530

    Subsets

--- Striver/Recursion/PowerSet.java
+++ Striver/Recursion/PowerSet.java
@@ -6,21 +6,42 @@ import java.util.ArrayList;
 public class PowerSet {
     public List<List<String>> powerSet(String str) {
         List<List<String>> result = new ArrayList<>();
-        generateSubsets(str, 0, new ArrayList<>(), result);
+        generateSubsetsStrings(str, 0, new ArrayList<>(), result);
         return result;
     }
 
-    private void generateSubsets(String str, int index, List<String> current,
+    private void generateSubsetsStrings(String str, int index, List<String> current,
             List<List<String>> result) {
         if (index == str.length()) {
             result.add(new ArrayList<>(current));
             return;
         }
 
-        generateSubsets(str, index + 1, current, result);
+        generateSubsetsStrings(str, index + 1, current, result);
 
         current.add(String.valueOf(str.charAt(index)));
-        generateSubsets(str, index + 1, current, result);
+        generateSubsetsStrings(str, index + 1, current, result);
+        current.remove(current.size() - 1);
+    }
+
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generateSubsets(nums, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void generateSubsets(int[] nums, int index, List<Integer> current,
+            List<List<Integer>> result) {
+        if (index == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        generateSubsets(nums, index + 1, current, result);
+
+        current.add(nums[index]);
+        generateSubsets(nums, index + 1, current, result);
         current.remove(current.size() - 1);
     }
 }
