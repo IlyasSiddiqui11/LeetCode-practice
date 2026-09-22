25edd56858bc2e0c0dd008514bf0d4647f898e7a
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-09-22 18:07:33 +0530

    .\.git\

---
+++ Striver/Recursion/CheckIfthereexistsasubsequencewithsumK.java
@@ -0,0 +1,23 @@
+package Striver.Recursion;
+
+public class CheckIfthereexistsasubsequencewithsumK {
+    public boolean checkSubsequenceSum(int[] nums, int k) {
+        if (nums.length == 0)
+            return k == 0;
+        return recursive(nums, nums.length - 1, k);
+    }
+
+    private boolean recursive(int[] nums, int index, int k) {
+        if (k == 0)
+            return true;
+        if (index == 0) {
+            return nums[index] == k;
+        }
+        boolean take = false;
+        boolean notTake = recursive(nums, index - 1, k);
+        if (k >= nums[index]) {
+            take = recursive(nums, index - 1, k - nums[index]);
+        }
+        return take || notTake;
+    }
+}
