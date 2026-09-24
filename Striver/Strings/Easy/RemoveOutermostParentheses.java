bfa4186f44ec82ae1a32b029691021c89afe1f01
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-09-24 19:48:40 +0530

    Remove Outermost Parentheses

--- Striver/Strings/Easy/RemoveOutermostParentheses.java
+++ Striver/Strings/Easy/RemoveOutermostParentheses.java
@@ -22,8 +22,21 @@ public class RemoveOutermostParentheses {
         }
         return ans;
     }
-    
+
     public String removeOuterParentheses(String s) {
-        
+        String ans = "";
+        int cnt = 0;
+        for(int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == ')') {
+                cnt--;
+            }
+            if (cnt > 0) {
+                ans += s.charAt(i);
+            }
+            if (s.charAt(i) == '(') {
+                cnt++;
+            }
+        }
+        return ans;
     }
 }
