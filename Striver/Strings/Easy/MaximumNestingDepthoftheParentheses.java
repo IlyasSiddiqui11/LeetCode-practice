a8ed556a3be4be28ceddc528b19752cdecfa84c0
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-09-24 20:24:40 +0530

    Maximum Nesting Depth of the Parentheses

---
+++ Striver/Strings/Easy/MaximumNestingDepthoftheParentheses.java
@@ -0,0 +1,17 @@
+package Striver.Strings.Easy;
+
+public class MaximumNestingDepthoftheParentheses {
+    public int maxDepth(String s){
+        int cnt = 0, ans = 0;
+        for(int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '(') {
+                cnt++;
+                ans = Math.max(ans, cnt);
+            }
+            else if (s.charAt(i) == ')') {
+                cnt--;
+            }
+        }
+        return ans;
+    }
+}
